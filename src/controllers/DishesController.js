881fd667c6206e00732a364e4201df04d0822df3
caio <caioeliasvieira2@gmail.com>
2024-03-08 13:58:49 -0300

    Updated DishesControler

--- src/controllers/DishesController.js
+++ src/controllers/DishesController.js
@@ -42,6 +42,64 @@ class DishesController {
             });
         }
     }
+      
+    async update(request, response) {
+        try {
+          const { id } = request.params;
+          const { title, description, price, category, ingredients } = request.body;
+          const imageFilename = request.file?.filename;
+      
+          const dish = await knex("dishes").where({ id }).first();
+      
+          if (!dish) {
+            throw new AppError("Prato não encontrado.", 404);
+          }
+      
+          const dishUpdate = {
+            title: title ?? dish.title,
+            description: description ?? dish.description,
+            category: category ?? dish.category,
+            price: price ?? dish.price,
+            updated_at: knex.fn.now(),
+          };
+      
+          if (imageFilename) {
+            const diskStorage = new DiskStorage();
+      
+            if (dish.image) {
+              await diskStorage.deleteFile(dish.image);
+            }
+      
+            const filename = await diskStorage.saveFile(imageFilename);
+            dishUpdate.image = filename;
+          }
+      
+          if (ingredients) {
+            await knex("ingredients").where({ dishes_id: id }).delete();
+      
+            const ingredientsArray = JSON.parse(ingredients || '[]');
+            const ingredientsInsert = ingredientsArray.map((name) => {
+              return {
+                dishes_id: id,
+                name,
+                user_id: request.user.id,
+              };
+            });
+      
+            await knex("ingredients").insert(ingredientsInsert);
+          }
+      
+          await knex("dishes").where({ id }).update(dishUpdate);
+      
+          return response.json({ message: "Prato atualizado com sucesso" });
+        } catch (error) {
+          console.error("Erro ao atualizar prato:", error);
+          return response.status(500).json({
+            status: "error",
+            message: "Erro interno no servidor",
+          });
+        }
+    }
 
     async show(request, response) {
         const { id } = request.params;
@@ -64,93 +122,50 @@ class DishesController {
     }
 
     async index(request, response) {
-        try {
-          const dishes = await knex("dishes")
-            .select([
-              "dishes.id",
-              "dishes.title",
-              "dishes.description",
-              "dishes.category",
-              "dishes.price",
-              "dishes.image",
-            ])
-            .orderBy("dishes.title");
-      
-          const dishesIngredients = await knex("ingredients");
-          const dishesWithIngredients = dishes.map((dish) => {
-            const dishIngredients = dishesIngredients.filter((ingredient) => ingredient.dish_id === dish.id);
-      
-            return {
-              ...dish,
-              ingredients: dishIngredients,
-            };
-          });
-      
-          return response.json(dishesWithIngredients);
-        } catch (error) {
-          console.error('Erro ao buscar pratos:', error);
-          return response.status(500).json({ error: 'Erro interno do servidor' });
-        }
-    }
+      const { title, ingredients } = request.query;
     
-    async update(request, response) {
-      try {
-        const { id } = request.params;
-        const { title, description, price, category, ingredients } = request.body;
-        const imageFilename = request.file?.filename;
+      let dishes;
     
-        const dish = await knex("dishes").where({ id }).first();
+      if (ingredients) {
+        const filterIngredients = ingredients
+          .split(",")
+          .map((ingredients) => ingredients.trim());
     
-        if (!dish) {
-          throw new AppError("Prato não encontrado.", 404);
-        }
-    
-        const dishUpdate = {
-          title: title ?? dish.title,
-          description: description ?? dish.description,
-          category: category ?? dish.category,
-          price: price ?? dish.price,
-          updated_at: knex.fn.now(),
-        };
-    
-        if (imageFilename) {
-          const diskStorage = new DiskStorage();
-    
-          if (dish.image) {
-            await diskStorage.deleteFile(dish.image);
-          }
-    
-          const filename = await diskStorage.saveFile(imageFilename);
-          dishUpdate.image = filename;
-        }
-    
-        if (ingredients) {
-          await knex("ingredients").where({ dishes_id: id }).delete();
-    
-          const ingredientsArray = JSON.parse(ingredients || '[]');
-          const ingredientsInsert = ingredientsArray.map((name) => {
-            return {
-              dishes_id: id,
-              name,
-              user_id: request.user.id,
-            };
-          });
-    
-          await knex("ingredients").insert(ingredientsInsert);
-        }
-    
-        await knex("dishes").where({ id }).update(dishUpdate);
-    
-        return response.json({ message: "Prato atualizado com sucesso" });
-      } catch (error) {
-        console.error("Erro ao atualizar prato:", error);
-        return response.status(500).json({
-          status: "error",
-          message: "Erro interno no servidor",
-        });
+        dishes = await knex("ingredients")
+          .select([
+            "dishes.id",
+            "dishes.title",
+            "dishes.user_id",
+            "dishes.category",
+            "dishes.price",
+            "dishes.description",
+            "dishes.image",
+          ])
+          .whereLike("dishes.title", `%${title}%`)
+          .whereIn("ingredients.name", filterIngredients)
+          .innerJoin("dishes", "dishes.id", "ingredients.dishes_id");
+      } else {
+        dishes = await knex("dishes")
+          .whereLike("title", `%${title}%`)
+          .orderBy("title");
       }
+    
+      const userIngredients = await knex("ingredients");
+      const dishesWithIngredients = dishes.map((dishe) => {
+        const dishesIngredients = userIngredients.filter(
+          (ingredient) => ingredient.dishes_id === dishe.id
+        );
+        return {
+          ...dishe,
+          ingredients: dishesIngredients,
+        };
+      });
+    
+      return response.json(dishesWithIngredients);
     }
     
-}
 
+    
+
+}
 module.exports = DishesController;
--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -14,48 +14,8 @@ dishesRoutes.use(ensureAuthenticated);
 
 dishesRoutes.get('/', dishesController.index);
 dishesRoutes.post('/', upload.single("image"), dishesController.create);
-dishesRoutes.patch("/:id", upload.single("image"), dishesController.update);
+dishesRoutes.patch("/:id", upload.single("image"),dishesController.update);
 dishesRoutes.get('/:id', dishesController.show);
 dishesRoutes.delete('/:id', dishesController.delete);
 
-// dishesRoutes.patch("/:id", upload.single("image"), (req, res) => {
-//     console.log(req.file.filename);
-//     response.json();
-// });
-
-// dishesRou tes.post("/", upload.single("image"), dishesController.create);
-
-// dishesRoutes.post("/image", upload.single("image"), (req, res) => {
-//     console.log(req.file);
-
-//     return res.json({ hello: "Rocket" });
-// });
-
-// dishesRoutes.patch("/image", upload.single("image"), dishImageController.update);
-// dishesRoutes.put("/:id", upload.single("image"), dishesController.update);
-
 module.exports = dishesRoutes;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function myMiddleware(request, response, next) {
-//     if(!request.body.isAdmin){
-//         return response.json({ message: "user unauthorized" });
-//     }
-
-//     next();
-// }
-
-// usersRoutes.post('/', myMiddleware, usersController.create)
